37a539f18bebbd14a769f3795a65539adfc66e1a
LwrncLiu <lwrncliu@gmail.com>
2023-01-06 21:39:17 -0600

    try running a node script instead of fetch

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3000
+// const express = require('express')
+// const app = express()
+// const port = 3000
 
-app.use(express.static('public'))
+// app.use(express.static('public'))
 
-app.listen(port, () => {
-    console.log(`App Listening at http://localhost:${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`App Listening at http://localhost:${port}`)
+// })
 
-app.get('/total_miles', async(request, response) => {
-    const auth = await reAuthorize()
-    const distance = await getDistance(auth)
-    response.json(distance)
-})
-
+// app.get('/total_miles', async(request, response) => {
+//     const auth = await reAuthorize()
+//     const distance = await getDistance(auth)
+//     response.json(distance)
+// })
 
 async function getDistance(res){
     const stats_link = `https://www.strava.com/api/v3/athletes/49030731/stats?access_token=${res.access_token}`
@@ -23,8 +22,8 @@ async function getDistance(res){
 
     const km_to_mile_factor = 0.621371
     const distance_miles = Math.round((stats.ytd_run_totals.distance * km_to_mile_factor) / 1000)
-
-    return distance_miles
+    console.log(distance_miles)
+    // return distance_miles
 }
 
 async function reAuthorize(){
@@ -45,3 +44,11 @@ async function reAuthorize(){
     const auth = await response.json()
     return auth
 }
+
+async function logDistance() {
+    const auth = await reAuthorize()
+    console.log(auth)
+    const distance = await getDistance(auth)
+}
+
+logDistance()
